51d7f3ba4c7e8f0fcd9e137b989edac9a8a6f574
Uttkarsh Patel <uttkarshpatel9@gmail.com>
2023-03-28 00:21:02 +0530

    : /

--- Backend/firebaseData.js
+++ Backend/firebaseData.js
@@ -1,16 +1,6 @@
 // import { initializeApp } from 'firebase/app';
 
 // // TODO: Replace the following with your app's Firebase project configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDWpUteq2Ui3oSoDO_5LtFZKh6lvOMfRvw",
-//     authDomain: "ontheroad-hackathon.firebaseapp.com",
-//     databaseURL: "https://ontheroad-hackathon-default-rtdb.firebaseio.com",
-//     projectId: "ontheroad-hackathon",
-//     storageBucket: "ontheroad-hackathon.appspot.com",
-//     messagingSenderId: "198382946882",
-//     appId: "1:198382946882:web:26b1345cfb1ccb48c3fbdb",
-//     measurementId: "G-T65Y6W15JV"
-// };
 
 // const firebaseApp = initializeApp(firebaseConfig);
 // export const db = getFirestore(firebaseApp);
